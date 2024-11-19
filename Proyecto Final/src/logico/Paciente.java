9b658f100fcc633fdb386ec2cc01c99b98703a5f
User <User@Diaz-Studio.lan>
2024-11-19 15:08:28 -0400

    Actualizando clase Paciente

--- Proyecto Final/src/logico/Paciente.java
+++ Proyecto Final/src/logico/Paciente.java
@@ -1,6 +1,15 @@
 package logico;
 
-public class Paciente 
-{
+public class Paciente {
+	
+	private String idCodPaciente;
+	
+	public String getIdCodPaciente() {
+		return idCodPaciente;
+	}
+
+	public void setIdCodPaciente(String idCodPaciente) {
+		this.idCodPaciente = idCodPaciente;
+	}
 	
 }
